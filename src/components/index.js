33c1d7fb25d612b2e22086fb848d6503bcd6e779
Olawale O Samson <smsnmicheal@gmail.com>
2022-05-20 01:10:36 +0100

    added fetch funtionality for apis in redux; added loading spinner

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 export { default as Carousel } from './Carousel'
 export { default as Footer } from './Footer'
 export { default as InputField } from './InputField'
+export { default as LoadingSpinner } from './LoadingSpinner'
 export { default as Modal } from './Modal'
 export { default as Navbar } from './Navbar'
 export { default as Sidebar } from './Sidebar'
--- src/redux/features/api/apiSlice.js
+++ src/redux/features/api/apiSlice.js
@@ -1,15 +1,34 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+
+const url = ''
 
 const initialState = {
     apis: [],
     isLoading: true
 }
 
+export const getApis = createAsyncThunk('apis/getApis', () => {
+    return fetch(url)
+    .then(res => res.json())
+    .catch(error => console.log(error))
+})
+
 const apiSlice = createSlice({
     name: 'apis',
     initialState,
-    reducers: {}
+    reducers: {},
+    extraReducers: {
+        [getApis.pending]: state => {
+            state.isLoading = true
+        },
+        [getApis.fulfilled]: (state, action) => {
+            state.isLoading = false
+            state.apis = action.payload
+        },
+        [getApis.rejected]: state => {
+            state.isLoading = false
+        }
+    }
 })
 
-export const { getSingleApi } = apiSlice.actions
 export default apiSlice.reducer
